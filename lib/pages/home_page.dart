9d3bb49f72cd9a35bf22a9531d6f73feee05cf33
Bryan <bryanevans890@gmail.com>
2026-06-11 20:59:49 -0300

    att homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'package:apifilme/models/filme.dart';
+import 'package:apifilme/services/omdb_service.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,6 +11,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+final TextEditingController controller = TextEditingController();
+
+final Omdbservice service = Omdbservice();
+
+List<Filme> filmes = [];
+
+bool carregando = false;
+
+Future<void> pesquisar() async {
+  setState(() {
+    carregando = true;
+  });
+  filmes = await service.buscarFilmes(controller.text,
+  );
+  setState(() {
+    carregando = false;
+  });
+}
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
