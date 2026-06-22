11fe27f4643e45b60910126eb158ff4ad9d29e71
Bryan <bryanevans890@gmail.com>
2026-06-22 15:14:54 -0300

    Finalização projeto

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,9 @@
-import 'dart:async';
-import 'package:apifilme/models/filme.dart';
-import 'package:apifilme/services/omdb_service.dart';
+
+
+import 'package:consulta_apii3/models/filme.dart';
+import 'package:consulta_apii3/services/omdb_service.dart';
 import 'package:flutter/material.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,27 +14,120 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-final TextEditingController controller = TextEditingController();
+  final TextEditingController controller = TextEditingController();
 
-final Omdbservice service = Omdbservice();
+  final OmdbService service = OmdbService();
 
-List<Filme> filmes = [];
+  List<Filme> filmes = [];
 
-bool carregando = false;
+  bool carregando = false;
 
-Future<void> pesquisar() async {
-  setState(() {
-    carregando = true;
-  });
-  filmes = await service.buscarFilmes(controller.text,
-  );
-  setState(() {
-    carregando = false;
-  });
-}
+  Future<void> pesquisar() async {
+    setState(() {
+      carregando = true;
+    });
 
-  @override
+    filmes = await service.buscarFilmes(controller.text);
+
+    setState(() {
+      carregando = false;
+    });
+  }
+
+
+ @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Buscar Filmes')),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                hintText: 'Digite o filme',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: pesquisar,
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            if (carregando) const CircularProgressIndicator(),
+            Expanded(
+              child: ListView.builder(
+                itemCount: filmes.length,
+                itemBuilder: (context, index) {
+                  final filme = filmes[index];
+                  return Card(
+                    elevation: 4,
+                    margin: const EdgeInsets.only(bottom: 12),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.network(
+                            filme.poster != 'N/A'
+                                ? filme.poster
+                                : 'https://via.placeholder.com/100',
+
+                            width: 100,
+                            height: 150,
+                            fit: BoxFit.cover,
+
+                            errorBuilder: (context, error, stackTrace) {
+                              return Container(
+                                width: 100,
+                                height: 150,
+                                color: Colors.grey[300],
+
+                                child: const Icon(
+                                  Icons.image_not_supported,
+                                  size: 40,
+                                ),
+                              );
+                            },
+                          ),
+                          const SizedBox(width: 10),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  filme.titulo,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 5),
+                                Text('Ano: ${filme.ano}'),
+                                const SizedBox(height: 10),
+                                Text(
+                                  filme.descricao,
+                                  maxLines: 5,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+  
+
--- lib/services/omdb_service.dart
+++ lib/services/omdb_service.dart
@@ -1,30 +1,133 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import '../models/filme.dart';
 
-class Omdbservice {
-  //final string apikey = 'SUA_CHAVE'
-  final String apikey = 'e539c6c2';
 
-  Future<List<Filme>> buscarFilmes(String nomeFilme) async {
-    List<Filme> listarFilmes = [];
+import 'package:consulta_apii3/models/filme.dart';
+import 'package:consulta_apii3/services/omdb_service.dart';
+import 'package:flutter/material.dart';
 
-    final urlBusca = 'http://www.omdbapi.com/?s=$nomeFilme&apikey=$apikey';
 
-    final responseBusca = await http.get(Uri.parse(urlBusca));
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
-    final dadosBusca = json.decode(responseBusca.body);
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
-    if (dadosBusca['Search'] != null) {
-      for (var item in dadosBusca['Search']) {
-        final imdbID = item['imdbID'];
-        final urlDetalhes =
-            'https://www.omdbapi/?i=$imdbID&plot=short&apikey=$apikey';
-        final responsedetalhes = await http.get(Uri.parse(urlDetalhes));
-        final dadosDetalhes = json.decode(responsedetalhes.body);
-        listarFilmes.add(Filme.fromJson(dadosDetalhes));
-      }
-    }
-    return listarFilmes;
+class _HomePageState extends State<HomePage> {
+
+  final TextEditingController controller = TextEditingController();
+
+  final OmdbService service = OmdbService();
+
+  List<Filme> filmes = [];
+
+  bool carregando = false;
+
+  Future<void> pesquisar() async {
+    setState(() {
+      carregando = true;
+    });
+
+    filmes = await service.buscarFilmes(controller.text);
+
+    setState(() {
+      carregando = false;
+    });
+  }
+
+
+ @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Buscar Filmes')),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                hintText: 'Digite o filme',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: pesquisar,
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            if (carregando) const CircularProgressIndicator(),
+            Expanded(
+              child: ListView.builder(
+                itemCount: filmes.length,
+                itemBuilder: (context, index) {
+                  final filme = filmes[index];
+                  return Card(
+                    elevation: 4,
+                    margin: const EdgeInsets.only(bottom: 12),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.network(
+                            filme.poster != 'N/A'
+                                ? filme.poster
+                                : 'https://via.placeholder.com/100',
+
+                            width: 100,
+                            height: 150,
+                            fit: BoxFit.cover,
+
+                            errorBuilder: (context, error, stackTrace) {
+                              return Container(
+                                width: 100,
+                                height: 150,
+                                color: Colors.grey[300],
+
+                                child: const Icon(
+                                  Icons.image_not_supported,
+                                  size: 40,
+                                ),
+                              );
+                            },
+                          ),
+                          const SizedBox(width: 10),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  filme.titulo,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 5),
+                                Text('Ano: ${filme.ano}'),
+                                const SizedBox(height: 10),
+                                Text(
+                                  filme.descricao,
+                                  maxLines: 5,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+  
+
